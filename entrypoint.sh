eb1a5632f9336c642421bb4ad8db1b5ca40eb389
George McCabe <23407799+georgemccabe@users.noreply.github.com>
2022-08-26 09:33:13 -0600

    test creating and uploading checksum

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,34 @@ if [ -z ${INPUT_TOKEN+x} ]; then
   exit 1
 fi
 
-echo GitHub repository: ${GITHUB_REPOSITORY}
-echo GitHub ref: ${GITHUB_REF}
-echo GitHub event_name: ${GITHUB_EVENT_NAME}
+repository=${GITHUB_REPOSITORY}
+ref=${GITHUB_REF}
+event_name=${GITHUB_EVENT_NAME}
+filename=`basename ${repository}`-`basename ${ref}`
 
-echo filename: `basename ${GITHUB_REPOSITORY}`-`basename ${GITHUB_REF}`.zip
+echo GitHub repository: ${repository}
+echo GitHub ref: ${ref}
+echo GitHub event_name: ${event_name}
+
+if [ "$event_name" != "release" ]; then
+    echo "Triggering event must be release!"
+    exit 1
+fi
+
+curl -LJO https://github.com/${repository}/archive/${ref}.zip
+curl -LJO https://github.com/${repository}/archive/${ref}.tar.gz
+
+ls ${filename}.zip
+ls ${filename}.tar.gz
+
+sha256sum ${filename}.zip > checksum.txt
+sha256sum ${filename}.tar.gz > checksum.txt
+
+cat checksum.txt
+
+curl \
+  -X PATCH \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: token ${INPUT_TOKEN}" \
+  https://api.github.com/repos/${repository}/releases/assets/99 \
+  -d '{"name":"checksum.txt","label":"checksum"}'
