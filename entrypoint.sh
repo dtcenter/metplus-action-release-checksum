6d67bf4674c84c95d4d6a08c012cc57b587458af
George McCabe <23407799+georgemccabe@users.noreply.github.com>
2022-08-26 10:17:36 -0600

    added logic to upload or replace checksum asset

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,11 @@
 #! /bin/sh
 
-# if INPUT_DOCKER_NAME is set, then run docker login
-if [ -z ${INPUT_TOKEN+x} ]; then
-  echo "GitHub Token must be passed to this action."
-  exit 1
-fi
-
 repository=${GITHUB_REPOSITORY}
 ref=${GITHUB_REF}
+tag=`basename ${ref}`
 event_name=${GITHUB_EVENT_NAME}
-filename=`basename ${repository}`-`basename ${ref}`
+dl_filename=`basename ${repository}`-${tag}
+cs_filename=checksum.txt
 
 echo GitHub repository: ${repository}
 echo GitHub ref: ${ref}
@@ -20,20 +16,66 @@ if [ "$event_name" != "release" ]; then
     exit 1
 fi
 
+###
+# create checksum
+###
+
+# download the assets containing the source code - zip and tar gz
 curl -LJO https://github.com/${repository}/archive/${ref}.zip
 curl -LJO https://github.com/${repository}/archive/${ref}.tar.gz
 
-ls ${filename}.zip
-ls ${filename}.tar.gz
+# check that files were downloaded correctly and match expected name
+ls ${dl_filename}.zip
+ls ${dl_filename}.tar.gz
 
-sha256sum ${filename}.zip > checksum.txt
-sha256sum ${filename}.tar.gz > checksum.txt
+# create checksum from both files
+echo Creating $cs_filename with checksums
+sha256sum ${dl_filename}.zip > $cs_filename
+sha256sum ${dl_filename}.tar.gz >> $cs_filename
 
-cat checksum.txt
+# print checksum values
+cat $cs_filename
 
-curl \
-  -X PATCH \
-  -H "Accept: application/vnd.github+json" \
-  -H "Authorization: token ${INPUT_TOKEN}" \
-  https://api.github.com/repos/${repository}/releases/assets/99 \
-  -d '{"name":"checksum.txt","label":"checksum"}'
+###
+# Upload or replace asset
+###
+
+# variables for curl commands to upload asset to release
+GH_API="https://api.github.com"
+GH_REPO="$GH_API/repos/$repository"
+GH_TAGS="$GH_REPO/releases/tags/$tag"
+AUTH="Authorization: token ${INPUT_TOKEN}"
+WGET_ARGS="--content-disposition --auth-no-challenge --no-cookie"
+CURL_ARGS="-LJO#"
+
+# Validate token
+curl -o /dev/null -sH "$AUTH" $GH_REPO || { echo "Error: Invalid repo, token or network issue!";  exit 1; }
+
+# Read asset tags
+response=$(curl -sH "$AUTH" $GH_TAGS)
+
+# Get ID of the release
+eval $(echo "$response" | grep -m 1 "id.:" | grep -w id | tr : = | tr -cd '[[:alnum:]]=')
+[ "$id" ] || { echo "Error: Failed to get release id for tag: $tag"; echo "$response" | awk 'length($0)<100' >&2; exit 1; }
+release_id="$id"
+
+# Get ID of the asset based on checksum filename
+id=""
+eval $(echo "$response" | grep -C1 "name.:.\+$cs_filename" | grep -m 1 "id.:" | grep -w id | tr : = | tr -cd '[[:alnum:]]=')
+assert_id="$id"
+if [ "$assert_id" = "" ]; then
+    echo "No need to overwrite asset"
+else
+    echo "Deleting asset($assert_id)... "
+    curl "$GITHUB_OAUTH_BASIC" -X "DELETE" -H "$AUTH" "https://api.github.com/repos/$repository/releases/assets/$assert_id"
+fi
+
+# Upload asset
+echo "Uploading asset... "
+
+# Construct url
+GH_ASSET="https://uploads.github.com/repos/$repository/releases/$release_id/assets?name=$(basename $cs_filename)"
+
+curl "$GITHUB_OAUTH_BASIC" --data-binary @"$cs_filename" -H "$AUTH" -H "Content-Type: application/octet-stream" $GH_ASSET
+
+echo end
