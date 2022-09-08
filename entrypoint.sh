cf33beea310e17d0762ec0532cad79d9debca30b
George McCabe <23407799+georgemccabe@users.noreply.github.com>
2022-09-08 12:19:22 -0600

    error and exit if asset cannot be uploaded properly

--- entrypoint.sh
+++ entrypoint.sh
@@ -85,5 +85,5 @@ for cs_filename in $cs_tar_filename $cs_zip_filename; do
     # Construct url
     GH_ASSET="https://uploads.github.com/repos/$repository/releases/$release_id/assets?name=$(basename $cs_filename)"
 
-    curl "$GITHUB_OAUTH_BASIC" --data-binary @"$cs_filename" -H "$AUTH" -H "Content-Type: application/octet-stream" $GH_ASSET
+    curl "$GITHUB_OAUTH_BASIC" --data-binary @"$cs_filename" -H "$AUTH" -H "Content-Type: application/octet-stream" $GH_ASSET  || { echo "Error: Could not upload asset ${cs_filename}!";  exit 1; }
 done
